86acc9a5965531113c13e912f97c2d082b8e0fc3
Raul Ospina <raminka13@hotmail.com>
2024-11-21 14:22:02 -0500

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://pennjet.net",
+  integrations: [tailwind(), partytown(), sitemap(), react()],
+});
